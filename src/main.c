b4a657fb6c63fcef600a91b6b32e99dbe76a329d
shihaoyu <shihaoyu@echo.tech>
2026-01-16 18:28:40 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char* CMD_EXIT = "exit";
+const char* CMD_ECHO = "echo";
+
+int echo(const char* command) { return 0; }
+
 int main(int argc, char* argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -14,11 +19,15 @@ int main(int argc, char* argv[]) {
       command[strcspn(command, "\n")] = '\0';
 
       // exit
-      if (strcmp("exit", command) == 0) {
+      if (strncmp(CMD_EXIT, command, strlen(CMD_EXIT)) == 0) {
         return 0;
-      }
-
-      if (strlen(command) > 0) {
+      } else if (strncmp(CMD_ECHO, command, strlen(CMD_ECHO)) == 0) {
+        int i = strlen(CMD_ECHO);
+        while (command[i] == ' ' && i < strlen(command)) {
+          i++;
+        }
+        printf("%s\n", &command[i]);
+      } else if (strlen(command) > 0) {
         printf("%s: command not found\n", command);
       }
     }
