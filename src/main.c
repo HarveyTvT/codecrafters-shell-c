579f3ef478a029d1be3c1fd982fc2f2ed41c8974
shihaoyu <shihaoyu@echo.tech>
2026-01-16 18:51:15 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* CMD_EXIT = "exit";
-const char* CMD_ECHO = "echo";
+static const char CMD_EXIT[] = "exit";
+static const char CMD_ECHO[] = "echo";
+static const char CMD_TYPE[] = "type";
+static const char* const CMDS[] = {CMD_EXIT, CMD_ECHO};
 
 int echo(const char* command) { return 0; }
 
@@ -18,7 +20,6 @@ int main(int argc, char* argv[]) {
       // remove new line
       command[strcspn(command, "\n")] = '\0';
 
-      // exit
       if (strncmp(CMD_EXIT, command, strlen(CMD_EXIT)) == 0) {
         return 0;
       } else if (strncmp(CMD_ECHO, command, strlen(CMD_ECHO)) == 0) {
@@ -27,6 +28,24 @@ int main(int argc, char* argv[]) {
           i++;
         }
         printf("%s\n", &command[i]);
+      } else if (strncmp(CMD_TYPE, command, strlen(CMD_TYPE)) == 0) {
+        int i = strlen(CMD_TYPE);
+        while (command[i] == ' ' && i < strlen(command)) {
+          i++;
+        }
+
+        int found = 0;
+        for (size_t j = 0; j < sizeof(CMDS) / sizeof(CMDS[0]); j++) {
+          if (strncmp(CMDS[j], &command[i], strlen(CMDS[j])) == 0) {
+            printf("%s is a shell builtin\n", &command[i]);
+            found = 1;
+            break;
+          }
+        }
+
+        if (found == 0) {
+          printf("%s: not found\n", &command[i]);
+        }
       } else if (strlen(command) > 0) {
         printf("%s: command not found\n", command);
       }
